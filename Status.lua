3f1c2315d2ab990daceb18f014a3b73da6850a46
Tran Van Bao <133487627+tranvanbao14112008@users.noreply.github.com>
2023-09-01 01:14:25 +0700

    Create Status.lua

---
+++ Status.lua
@@ -0,0 +1,44 @@
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/miroeramaa/TurtleLib/main/TurtleUiLib.lua"))()
+
+
+local window = library:Window("Pickle Hub | Main Status")
+
+window:Label("Dingus", Color3.fromRGB(127, 143, 166))
+window:Label("Zombie Attack", Color3.fromRGB(127, 143, 166))
+window:Label("Doors", Color3.fromRGB(127, 143, 166))
+window:Label("Anime Fighting Simulator X", Color3.fromRGB(127, 143, 166))
+
+
+
+local window = library:Window("Pickle Hub | UGC Status")
+
+window:Label("Race Clicker", Color3.fromRGB(127, 143, 166))
+window:Label("Dungeon Quest", Color3.fromRGB(127, 143, 166))
+window:Label("Chest Hero Simulator", Color3.fromRGB(127, 143, 166))
+window:Label("Anime Energy Clash Simulator", Color3.fromRGB(127, 143, 166))
+
+local window = library:Window("Pickle Hub | Misc")
+
+window:Button("Destroy Ui", function()
+   library:Destroy()
+end)
+
+window:Button("Fly Gui", function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/tranvanbao14112008/SupportBuLon/main/FlyBuLon"))()
+end)
+
+window:Button("Anti Lag", function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/tranvanbao14112008/ScriptLinhTinh/main/AntiLag2"))()
+end)
+
+window:Button("Hitbox", function()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/tranvanbao14112008/SupportBuLon/main/Hitbox.lua"))()
+end)
+
+window:Button("Link Youtube", function()
+   setclipboard("https://youtube.com/@tranvanbao1411")
+end)
+
+window:Button("Link Discord", function()
+   setclipboard("https://discord.gg/ta7EasMvCv")
+end)
