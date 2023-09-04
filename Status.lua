926e3e2dfbfb5fd50b4a061262a987af36947c25
Tran Van Bao <133487627+tranvanbao14112008@users.noreply.github.com>
2023-09-04 21:56:36 +0700

    Update Status.lua

--- Status.lua
+++ Status.lua
@@ -3,7 +3,6 @@ local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/miroe
 
 local window = library:Window("Pickle Hub | Status")
 
-window:Label("Dingus", Color3.fromRGB(127, 143, 166))
 window:Label("Dungeon Quest", Color3.fromRGB(127, 143, 166))
 window:Label("Chest Hero Simulator", Color3.fromRGB(127, 143, 166))
 window:Label("Anime Energy Clash Simulator", Color3.fromRGB(127, 143, 166))
