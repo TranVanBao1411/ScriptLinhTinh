66a1c628079047d17424dd08412a00832fc99326
Tran Van Bao <133487627+tranvanbao14112008@users.noreply.github.com>
2023-09-05 12:07:57 +0700

    Update Status.lua

--- Status.lua
+++ Status.lua
@@ -1,11 +1,1 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/miroeramaa/TurtleLib/main/TurtleUiLib.lua"))()
 
-
-local window = library:Window("Pickle Hub | Status")
-
-window:Label("Dungeon Quest", Color3.fromRGB(127, 143, 166))
-window:Label("Chest Hero Simulator", Color3.fromRGB(127, 143, 166))
-window:Label("Anime Energy Clash Simulator", Color3.fromRGB(127, 143, 166))
-window:Button("Destroy Ui", function()
-   library:Destroy()
-end)
